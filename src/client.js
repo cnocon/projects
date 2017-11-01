32f05aabb7abd7d742f4c0a6dfd4ee0bc5df107d
Cristin O'Connor <cristin@cristinoconnor.com>
2017-11-01 15:22:33 -0500

    Set up React dependencies in src file, along with a sample (terribly done) component. * Updates to some node modules included.

--- src/client.js
+++ src/client.js
@@ -1,1 +1,13 @@
-console.log('the js file to edit is src/client.js');
+// importing external resources by using `import` directive, and specifying the class names and node package names for what you need. E.g. import ReactDom class from 'react-dom' module in node
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const component = <div className="jumbotron jumbotron-fluid text-sm-center">
+  <div className="container">
+      <h1>Building React User Interfaces with Bootstrap and SASS</h1>
+      <p></p>
+      <p><a className="btn btn-primary" href="http://02geek.com" target="_blank">Discover Things</a></p>
+    </div>
+  </div>;
+
+ReactDOM.render(component, document.getElementById('root'));
