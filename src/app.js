f74586aaec038e85d6941a6928a5d397bb737def
Cristin O'Connor <cristin@cristinoconnor.com>
2017-11-01 17:07:18 -0500

    Created a dynamic JSX element (can create a different type of tag depending on some condition(s) you specify.

--- src/app.js
+++ src/app.js
@@ -4,11 +4,12 @@ import Button from './bootstrap/button';
 export default class App extends React.Component {
   render() {
     return (
-      <div className="jumbotron jumbotron-fluid text-sm-center">
+      <div className="jumbotron jumbotron-fluid text-sm-center" style={{height:'100vh'}}>
         <div className="container">
           <h1>Building React User Interfaces with Bootstrap and SASS</h1>
-          <Button href="http://cristin.io" target="_blank" className="btn btn-primary"/>
-          <Button href="mailto:cristin@cristinoconnor.com"  className="btn btn-secondary"/>
+          <p>with Bootstrap and SASS</p>
+          <Button type="button" value="Does Nothing" className="btn-danger" />
+          <Button href="http://cristin.io" label="Code Examples" className="btn-success ml-2" target="_blank"/>
         </div>
       </div>
     );
--- src/bootstrap/button.js
+++ src/bootstrap/button.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default class Button extends React.Component {
   render () {
+    const Tag = this.props.value ? 'button' : 'a';
+    const className = this.props.className + ' btn';
+    const text = this.props.value || this.props.label;
     return (
-      <a {...this.props}>{this.props.href}</a>
+      <Tag {...this.props} className={className}>{text}</Tag>
     );
   }
 }
-
