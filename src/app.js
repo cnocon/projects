aba7ea69f535311efe3fda9fac6c84533874522d
Cristin O'Connor <cristin@cristin.io>
2017-11-02 13:05:05 -0500

    Added an onClick handler to a component and used constructor with super.

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ import Jumbotron from './bootstrap/jumbotron';
 import './app.scss';
 
 export default class App extends React.Component {
+  // within the context of the constructor, we can make sure a react component knows how to fetch and handle props, so we're going to call for the parent via super
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick(e) {
+    console.log(this); // 'this' is undefined since we are IN THE PROCESS OF CREATING A NEW OBJECT; `this` is not a thing yet. To fix this you can use a constructor,
+  }
   render() {
     return (
       <CSSTransitionGroup transitionName='app' transitionAppearTimeout={500} transitionAppear={true} transitionLeaveTimeout={500} transitionEnterTimeout={500}>
@@ -15,7 +23,7 @@ export default class App extends React.Component {
           <p className="lead">with Bootstrap and SASS</p>
           <Button href="http://cristin.io" role="button" target="_blank" label="Code" className="-primary -lg"/>
           <Button href="mailto:cristin@cristin.io" role="button" target="_blank" label="Contact" className="-primary -lg" />
-          <Button type="button" className="-danger rounded rounded-circle text-light -lg" value="&hearts;"/>
+          <Button onClick={this.onClick} type="button" className="-danger rounded rounded-circle text-light -lg" value="&hearts;"/>
         </Jumbotron>
       </CSSTransitionGroup>
     );
