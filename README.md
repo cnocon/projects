7440c9e5e8d6522b2cdb092832dd5787e5da9f3d
Cristin O'Connor <cristin@cristin.io>
2017-11-02 01:18:33 -0500

    Installed some CSS loader packages to prepare for SASS section of lecture, and also added some troubleshooting tips to a new README file.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Troubleshooting Package Installs
+
+A lot of time it's because my version of node that gets used by default is 9.0.0. 
+
+To fix this, just use Node Version Manager to switch the local version.
+
+```shell
+$ nvm use 8.0 #or whatever version of node you need
+```
+
+If it's missing, run `$ nvm install 8.0.0` or whatever version you need. 
+
+---
--- src/bootstrap/jumbotron.js
+++ src/bootstrap/jumbotron.js
@@ -8,13 +8,11 @@ export default class Jumbotron extends React.Component {
     const props = Object.assign({}, this.props);
           props.className = classify(CSS_NAME, props.className);
     const containerFluid = props.containerFluid;
-     // remove invalid html attr from props spread
     delete props.containerFluid;
-    // it's dumb we have the containerFluid property getting passed to the component at all if we can just tell by the use of one of the classNames or not... but i think the point of this segment/commit is to just see how you can use a parent component to set a child components state, WITH OR WITHOUT a non-className property (even if that non-className property isn't a valid html attribute)
     const body = props.className.includes(CSS_NAME+'-fluid') ?
       <Container fluid={containerFluid}>{this.props.children}</Container> : this.props.children;
     return (
-      <div {...props}>{ body }</div>
+      <div {...props}>{body}</div>
     );
   }
 }
