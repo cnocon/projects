c4ceb622666796b3e29125a2ee4833e38b81a087
Cristin O'Connor <cristin@cristin.io>
2017-11-02 08:34:17 -0500

    Installed an animation package and also created an app.scss file to serve as a kind of 'core' or 'backbone' that imports all the sass modules needed, plus custom styles.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
-import './styles.scss';
+import './app.scss';
 
 export default class App extends React.Component {
   render() {
