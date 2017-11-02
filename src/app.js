80de5b6d65fd3993612c3fc4e3a4507654c97e0d
Cristin O'Connor <cristin@cristin.io>
2017-11-01 22:42:13 -0500

    Created a 'utility package' for bootstrap, starting with the  method we'd previously added to the button component class.

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import Button from './bootstrap/button';
 export default class App extends React.Component {
   render() {
     return (
-      <div className="jumbotron jumbotron-fluid text-sm-center" style={{height:'100vh'}}>
+      <div className="jumbotron jumbotron-fluid text-sm-center">
         <div className="container">
           <h1 className="font-weight-light display-4">Building React User Interfaces with Bootstrap and SASS</h1>
           <p className="lead mb-5">with Bootstrap and SASS</p>
           <Button href="http://cristin.io" label="Code Examples" className="-primary ml-2" target="_blank"/>
-          <Button href="mailto:cristin@cristinoconnor.com" label="Contact Me" className="-primary ml-2" />
+          <Button href="mailto:cristin@cristin.io" label="Contact Me" className="-primary ml-2" />
           <p><Button type="button" value="I'm Useless" className="-secondary mx-auto w-60 mt-4" /></p>
         </div>
       </div>
--- src/bootstrap/button.js
+++ src/bootstrap/button.js
@@ -1,19 +1,10 @@
 import React from 'react';
+import {classify} from './utils'
 
 export default class Button extends React.Component {
-  classify (key, value) {
-    const terms = value.split(' ');
-    let classes = 'btn';
-    while (terms.length > 0) {
-      const text = terms.pop();
-      const newClass = /^-/.test(text) ? `${key}${text}` : text;
-      classes += ` ${newClass}`;
-    }
-    return classes;
-  }
   render () {
     const Tag = this.props.value ? 'button' : 'a';
-    const classNames = this.classify('btn', this.props.className);
+    const classNames = classify('btn', this.props.className);
     const text = this.props.value || this.props.label;
     return (
       <Tag {...this.props} className={classNames}>{text}</Tag>
