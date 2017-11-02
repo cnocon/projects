7ecf646eab2318f318a88ac0278ed32100065ec5
Cristin O'Connor <cristin@cristin.io>
2017-11-02 00:20:17 -0500

    Learned how to handle it when there's an error in the console due to sending a property to a component that is NOT a valid html attribute (since we were using the spread operator on this.props in the component class definition)

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import Jumbotron from './bootstrap/jumbotron';
 export default class App extends React.Component {
   render() {
     return (
-      <Jumbotron className="jumbotron jumbotron-fluid text-sm-center" style={{height:"100vh"}}>
+      <Jumbotron className="jumbotron jumbotron-fluid text-sm-center" style={{height:"100vh"}} containerFluid={true}>
           <h1 className="display-2">Building React User Interfaces</h1>
           <p className="lead mb-5 display-4">with Bootstrap and SASS</p>
           <Button href="//cristin.io" label="Code Examples" className="-primary -lg mr-4" role="button" target="_blank"/>
--- src/bootstrap/jumbotron.js
+++ src/bootstrap/jumbotron.js
@@ -4,8 +4,14 @@ import Container from './container';
 
 export default class Jumbotron extends React.Component {
   render() {
+    // this will help us fix the fact that a prop passed (containerFluid) is NOT a valid html attribute (and we're using the spread operater, '...' to insert html attributes in the return element (note we changed the reference from `this.props` to the new `props` variable, and is causing an error in the console
+    const props = Object.assign({}, this.props);
+    const containerFluid = props.containerFluid;
+    // now delete the property that is causing the invalid html error
+    delete props.containerFluid;
     return (
-      <div {...this.props} className={classify('jumbotron', this.props.className)}>
+      // we reference the props const now, instead of this.props`
+      <div {...props} className={classify('jumbotron', this.props.className)}>
         <Container className="text-white p-4" fluid>
           {this.props.children}
         </Container>
