a5c0109b87cfd0095872690faaecab53f3aae7b4
Cristin O'Connor <cristin@cristin.io>
2017-11-02 00:13:12 -0500

    Extended the Jumbotron component class by creating a new component class called Container, that gets included in the jumbotron.js package file.

--- src/app.js
+++ src/app.js
@@ -6,13 +6,11 @@ export default class App extends React.Component {
   render() {
     return (
       <Jumbotron className="jumbotron jumbotron-fluid text-sm-center" style={{height:"100vh"}}>
-        <div className="container text-white p-4">
           <h1 className="display-2">Building React User Interfaces</h1>
           <p className="lead mb-5 display-4">with Bootstrap and SASS</p>
           <Button href="//cristin.io" label="Code Examples" className="-primary -lg mr-4" role="button" target="_blank"/>
           <Button href="mailto:cristin@cristin.io" label="Contact Me" className="-primary -lg" />
           <p className="mt-4"><Button type="button" value="&hearts;" className="-secondary text-primary -lg" /></p>
-        </div>
       </Jumbotron>
     );
   }
--- src/bootstrap/jumbotron.js
+++ src/bootstrap/jumbotron.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {classify} from './utils';
+import Container from './container';
 
 export default class Jumbotron extends React.Component {
   render() {
     return (
       <div {...this.props} className={classify('jumbotron', this.props.className)}>
-        {this.props.children}
+        <Container className="text-white p-4" fluid>
+          {this.props.children}
+        </Container>
       </div>
     );
   }
