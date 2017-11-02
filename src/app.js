2fab754612a079bd0f2ae8ae3f0c52a2bf2c81ed
Cristin O'Connor <cristin@cristin.io>
2017-11-02 11:18:47 -0500

    Added an animation plugin that allowed us to wrap our jumbotron component in an animation.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
 import './app.scss';
@@ -6,13 +7,15 @@ import './app.scss';
 export default class App extends React.Component {
   render() {
     return (
-      <Jumbotron className="-fluid" containerFluid={true}>
-        <h1 className="display-3">Building React User Interfaces</h1>
-        <p className="lead">with Bootstrap and SASS</p>
-        <Button href="http://cristin.io" role="button" target="_blank" label="Code Examples" className="-primary -lg"/>
-        <Button href="mailto:cristin@cristin.io" role="button" target="_blank" label="Contact Me" className="-primary -lg" />
-        <Button type="button" value="&hearts;" className="-light -lg text-primary" />
-      </Jumbotron>
+      <ReactCSSTransitionGroup transitionName='app' transitionAppear={true} transitionAppearTimeout={500}>
+        <Jumbotron className="-fluid" containerFluid={true}>
+          <h1 className="display-3">Building React User Interfaces</h1>
+          <p className="lead">with Bootstrap and SASS</p>
+          <Button href="http://cristin.io" role="button" target="_blank" label="Code Examples" className="-primary -lg"/>
+          <Button href="mailto:cristin@cristin.io" role="button" target="_blank" label="Contact Me" className="-primary -lg" />
+          <Button type="button" value="&hearts;" className="-light -lg text-primary" />
+        </Jumbotron>
+      </ReactCSSTransitionGroup>
     );
   }
 }
