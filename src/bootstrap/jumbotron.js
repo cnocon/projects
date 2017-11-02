26c0731fab3093d1d03fdc2116e312f1857e96a2
Cristin O'Connor <cristin@cristin.io>
2017-11-02 00:23:39 -0500

    Learned how to dynamically render a 'sub component' like the Container component, if the parent needs it (if the Jumbotron component has thd the 'containerFluid' property).
      * DYNAMIC CHILD COMPONENTS.
      * it's dumb we have the containerFluid property getting passed to the component at all if we can just tell by the use of one of the classNames or not... but i think the point of this segment/commit is to just see how you can use a parent component to set a child components state, WITH OR WITHOUT a non-className property (even if that non-className property isn't a valid html attribute)

--- src/bootstrap/jumbotron.js
+++ src/bootstrap/jumbotron.js
@@ -4,18 +4,17 @@ import Container from './container';
 
 export default class Jumbotron extends React.Component {
   render() {
-    // this will help us fix the fact that a prop passed (containerFluid) is NOT a valid html attribute (and we're using the spread operater, '...' to insert html attributes in the return element (note we changed the reference from `this.props` to the new `props` variable, and is causing an error in the console
+    const CSS_NAME = 'jumbotron';
     const props = Object.assign({}, this.props);
+          props.className = classify(CSS_NAME, props.className);
     const containerFluid = props.containerFluid;
-    // now delete the property that is causing the invalid html error
+     // remove invalid html attr from props spread
     delete props.containerFluid;
+    // it's dumb we have the containerFluid property getting passed to the component at all if we can just tell by the use of one of the classNames or not... but i think the point of this segment/commit is to just see how you can use a parent component to set a child components state, WITH OR WITHOUT a non-className property (even if that non-className property isn't a valid html attribute)
+    const body = props.className.includes(CSS_NAME+'-fluid') ?
+      <Container fluid={containerFluid}>{this.props.children}</Container> : this.props.children;
     return (
-      // we reference the props const now, instead of this.props`
-      <div {...props} className={classify('jumbotron', this.props.className)}>
-        <Container className="text-white p-4" fluid>
-          {this.props.children}
-        </Container>
-      </div>
+      <div {...props}>{ body }</div>
     );
   }
 }
