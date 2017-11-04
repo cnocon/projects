e7e2af42daeb8c842957575dd3aeeda555c6e2b5
Cristin O'Connor <cristin@cristin.io>
2017-11-04 15:46:45 -0500

    Added onClick method to log coordinates of click event.

--- source/components/map.js
+++ source/components/map.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 export default class Map extends React.Component {
+  onClick(e) {
+    if (true) {
+      let img = e.target,
+          point = {
+            x: e.offsetX || e.pageX - e.target.offsetLeft,
+            y: e.offsetY || e.pageY - e.target.offsetTop
+          };
+      console.log(point);
+    }
+  }
   render () {
     return (
-      <img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Earthmap1000x500.jpg" />
+      <img onClick={this.onClick} src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Earthmap1000x500.jpg" />
     )
   }
-
 }
