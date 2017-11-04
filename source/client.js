e0fd1d5d2f423d86c868406a545d85b5e68a30c4
Cristin O'Connor <cristin@cristin.io>
2017-11-04 17:57:45 -0500

    Imported dataset and reformatted it... however it's all terribly done. But at least we imported stuff from a CSV? I guess that's the lesson?

--- source/client.js
+++ source/client.js
@@ -1,30 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chart from './components/chart';
+import data2 from '../data/data.csv';
+
+let obj = data2.shift(),
+  formattedData = {labels: [obj.name], datasets:[]},
+  map = new Map(); //this is an ES6 feature
+delete obj.name;
+for(var item in obj) {
+  let ds = {label: item, data: [obj.item]};
+  formattedData.datasets.push(ds);
+  map[item] = ds;
+};
+
+data2.forEach((obj, i, a) => {
+  formattedData.labels.push(obj.name);
+  delete obj.name;
+  for(let item in obj) {
+    map[item].data.push(obj[item]);
+  }
+});
 
 const data = {
   type: 'bar',
-  data: {
-    labels: ["Ben", "James", "Marry", "Sam", "Gloria"],
-    datasets: [
-      {
-        label: 'Fun',
-        data:[80,40,21,44,94],
-        backgroundColor: "rgba(255,0,0,0.2)",
-        borderColor: 'red',
-        borderWidth:1
-      },
-      {
-        label: 'Smart',
-        data:[90,30,90,90,100],
-        backgroundColor: "rgba(0,0,255,0.2)",
-        borderColor: "blue",
-        borderWidth:1
-      }
-    ]
-  },
+  data: formattedData,
   options: {}
-}
+};
 
 ReactDOM.render(<Chart {...data} />, document.getElementById('root'));
 
+
+// old data:
+// const oldData = {
+//   type: 'bar',
+//   data: {
+//     labels: ["Ben", "James", "Marry", "Sam", "Gloria"],
+//     datasets: [{
+//       label: 'Fun',
+//       data:[80,40,21,44,94],
+//       backgroundColor: "rgba(255,0,0,0.2)",
+//       borderColor: 'red',
+//       borderWidth:1
+//     }, {
+//       label: 'Smart',
+//       data:[90,30,90,90,100],
+//       backgroundColor: "rgba(0,0,255,0.2)",
+//       borderColor: "blue",
+//       borderWidth:1
+//     }]
+//   },
+//   options: {}
+// }
--- source/components/chart.js
+++ source/components/chart.js
@@ -3,10 +3,10 @@ import ChartJS from 'Chart.js';
 
 export default class Chart extends React.Component {
   componentDidMount() {
-    const ctx = this.refs['chart'].getContext('2d');
+    const context = this.refs['chart'].getContext('2d');
     let {type, data, options} = this.props;
 
-    this.chart = new ChartJS(ctx, {
+    this.chart = new ChartJS(context, {
       type: type,
       data: data,
       options: options
