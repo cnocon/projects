fec0f1528cbaba6bdd65014f6c804d2e57b84792
Cristin O'Connor <cristin@cristin.io>
2017-11-04 15:11:29 -0500

    * Integrated JSON into react components * Changing the data in the data.json file changes the values in your component. * Learned how to import json data using webpack.

--- source/client.js
+++ source/client.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import data from "./data.json";
+import data from './data.json';
 import App from './components/app';
 
-// document.body.innerText = data.name;
-ReactDOM.render(<App/>, document.getElementById('react'));
+ReactDOM.render(
+  <App {...data} />,
+  document.getElementById('root')
+);
 
--- source/components/app.js
+++ source/components/app.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default class App extends React.Component {
   render () {
+    var smartMessage = this.props.smart ? "She's smart." : "She's not smart.";
     return (
-      <h1>Hello world.</h1>
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>She is at least {this.props.age} years old. {smartMessage}.</p>
+      </div>
     )
   }
 }
