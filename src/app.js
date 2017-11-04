51d7692c7c1c1a48d7d0b828c96a2ff60ff1e132
Cristin O'Connor <cristin@cristin.io>
2017-11-04 13:46:05 -0500

    Created a reset button to reset buttons and count; added a leave animation

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.onClick = this.onClick.bind(this);
-    // the only time you directly set state is here. every other time you have to call this.setState(blah) or else React won't recognize that there was a state change!! Using this.setState lets react know something changed and it needs to call the render method again.
+    this.onReset = this.onReset.bind(this);
     this.state = {
       clickCount: 0,
       labels: []
@@ -21,24 +21,27 @@ export default class App extends React.Component {
       labels: labels
     });
   }
+  onReset(e) {
+    this.setState({clickCount: 0, labels: []});
+  }
   render() {
     let buttons = this.state.labels.map(
-      // arrow functions automatically have this as component context?
       (value, i) => {
-        // you have to give it a key if you are creating elements dynamically
-        return <Button key={value} value={`${value}`} type="button" className="-info -lg"/>
+        return <Button key={i} value={`${value}`} type="button" className="-info -lg"/>
       }
     );
     return (
       <Jumbotron className="-fluid" containerFluid={true}>
-        <h1 className="display-3">Building React User Interfaces</h1>
-        <p className="lead">with Bootstrap and SASS</p>
-        <Button href="http://cristin.io" role="button" target="_blank" label="Code" className="-primary -lg"/>
-        <Button href="mailto:cristin@cristin.io" role="button" target="_blank" label="Contact" className="-primary -lg" />
-        <Button onClick={this.onClick} type="button" className="-success rounded rounded-circle -lg" value={this.state.clickCount || '0'}/>
-      <CSSTransitionGroup transitionName='buttons'transitionLeaveTimeout={5000} transitionEnterTimeout={5000}>{buttons}</CSSTransitionGroup>
-      </Jumbotron>
+        <h1 className="display-3">Building React User Interfaces</h1><p className="lead">with Bootstrap and SASS</p>
 
+        <Button href="mailto:cristin@cristin.io" role="button" target="_blank" label="Contact" className="-primary -lg" />
+
+        <Button onClick={this.onClick} type="button" className="-light" value={`${this.state.clickCount} Clicks`}/>
+
+        <Button onClick={this.onReset} type="button" className="-danger -lg" value="Reset"/>
+        <br/><br/>
+        <CSSTransitionGroup transitionName='buttons' transitionLeaveTimeout={500} transitionEnterTimeout={2000}>{buttons}</CSSTransitionGroup>
+      </Jumbotron>
     );
   }
 }
