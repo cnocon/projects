07fab68d7de39995984d40be42961bf8fd9d127d
Cristin O'Connor <cristin@cristin.io>
2017-11-04 16:41:54 -0500

    installed react-griddle to turn our map into a grid. we set up xhr to load cities into Griddle react component.

--- source/components/map.js
+++ source/components/map.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import Griddle from 'griddle-react';
 
 export default class Map extends React.Component {
+  constructor(props) {
+    // gets React.Component props
+    super(props);
+    // below line always binds scope here to all of them
+    this.onClick = this.onClick.bind(this);
+    this.state = {
+      cities: []
+    }
+  }
   onClick(e) {
     let img = e.target,
         latSize = img.height / 180,
@@ -9,30 +19,37 @@ export default class Map extends React.Component {
           x: e.offsetX || e.pageX - e.target.offsetLeft,
           y: e.offsetY || e.pageY - e.target.offsetTop
         },
-        lat, lon,
-        r = 1;
+        lat = -1 * (point.y - (img.height / 2)) / latSize,
+        lon = (point.x - (img.width / 2)) / lonSize,
+        r = 1,
+        endpoint = `http://api.geonames.org/citiesJSON?formatted=true&north=${lat+r}&south=${lat-r}&east=${lon+r}&west=${lon-r}&lang=en&username=geek&style=full`;
 
-    lat = -1 * (point.y - (img.height / 2)) / latSize;
-    lon = (point.x - (img.width / 2)) / lonSize;
-
-    var ws = `http://api.geonames.org/citiesJSON?formatted=true&north=${lat+r}&south=${lat-r}&east=${lon+r}&west=${lon-r}&lang=en&username=demo&style=full`;
 
     // call the web service by creating new Xml http request
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', ws);
+
+    xhr.open('GET', endpoint);
+
+    // arrow functions capture this as what's outside of it
     xhr.onload = () => {
       if (xhr.status === 200) {
-        console.log(xhr.responseText);
+        this.setState({
+          cities: JSON.parse(xhr.responseText).geonames
+        });
       } else {
-        console.log('error');
+        // Error
       }
     };
     xhr.send();
+
   }
 
   render () {
     return (
-      <img onClick={this.onClick} src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Earthmap1000x500.jpg" />
+      <div>
+        <img onClick={this.onClick} src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Earthmap1000x500.jpg"/>
+        <Griddle data={this.state.cities} />
+      </div>
     )
   }
 }
