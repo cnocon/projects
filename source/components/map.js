5fbf33429b83c397693cb2583d12d3837a1d5d33
Cristin O'Connor <cristin@cristin.io>
2017-11-04 16:02:28 -0500

    got endpoint for geographic location based on coordinates, but nothing is hooked up yet.

--- source/components/map.js
+++ source/components/map.js
@@ -10,12 +10,18 @@ export default class Map extends React.Component {
             x: e.offsetX || e.pageX - e.target.offsetLeft,
             y: e.offsetY || e.pageY - e.target.offsetTop
           },
-          lat, lon;
+          lat, lon,
+          r = 1; // range of 1 degree
 
       lat = -1 * (point.y - (img.height / 2)) / latSize;
       lon = (point.x - (img.width / 2)) / lonSize;
+
+      console.log(lat, lon);
+      var ws = `http://api.geonames.org/citiesJSON?formatted=true&north=${lat+r}&south=${lat-r}&east=${lon+r}&west=${lon-r}&lang=en&username=demo&style=full`;
+      console.log(ws);
     }
   }
+
   render () {
     return (
       <img onClick={this.onClick} src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Earthmap1000x500.jpg" />
