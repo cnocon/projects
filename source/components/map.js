dbda5ce9299387f2eaffecb86a6a8bed16d27efa
Cristin O'Connor <cristin@cristin.io>
2017-11-04 16:17:54 -0500

    retrieved json response from service by creating a XMLHttpRequest object

--- source/components/map.js
+++ source/components/map.js
@@ -2,24 +2,32 @@ import React from 'react';
 
 export default class Map extends React.Component {
   onClick(e) {
-    if (true) {
-      let img = e.target,
-          latSize = img.height / 180, // calculate size of latitude
-          lonSize = img.width / 360, // size of longitude
-          point = {
-            x: e.offsetX || e.pageX - e.target.offsetLeft,
-            y: e.offsetY || e.pageY - e.target.offsetTop
-          },
-          lat, lon,
-          r = 1; // range of 1 degree
+    let img = e.target,
+        latSize = img.height / 180,
+        lonSize = img.width / 360,
+        point = {
+          x: e.offsetX || e.pageX - e.target.offsetLeft,
+          y: e.offsetY || e.pageY - e.target.offsetTop
+        },
+        lat, lon,
+        r = 1;
 
-      lat = -1 * (point.y - (img.height / 2)) / latSize;
-      lon = (point.x - (img.width / 2)) / lonSize;
+    lat = -1 * (point.y - (img.height / 2)) / latSize;
+    lon = (point.x - (img.width / 2)) / lonSize;
 
-      console.log(lat, lon);
-      var ws = `http://api.geonames.org/citiesJSON?formatted=true&north=${lat+r}&south=${lat-r}&east=${lon+r}&west=${lon-r}&lang=en&username=demo&style=full`;
-      console.log(ws);
-    }
+    var ws = `http://api.geonames.org/citiesJSON?formatted=true&north=${lat+r}&south=${lat-r}&east=${lon+r}&west=${lon-r}&lang=en&username=demo&style=full`;
+
+    // call the web service by creating new Xml http request
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', ws);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else {
+        console.log('error');
+      }
+    };
+    xhr.send();
   }
 
   render () {
