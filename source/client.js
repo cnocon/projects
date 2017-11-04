492476198a2720964621920548f1bca241c18670
Cristin O'Connor <cristin@cristin.io>
2017-11-04 17:11:57 -0500

    created data for chart component... but it's just a var in the client file

--- source/client.js
+++ source/client.js
@@ -2,5 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Chart from './components/chart';
 
-ReactDOM.render(<Chart />, document.getElementById('root'));
+const data = {
+  type: 'bar',
+  data: {
+    labels: ["Ben", "James", "Marry", "Sam", "Gloria"],
+    dataset: [
+      {
+        label: 'Fun',
+        data:[80,40,21,44,94],
+        borderColor: "rgba(255,0,0,0.2)",
+        borderWidth:1
+      },
+      {
+        label: 'Smart',
+        data:[90,30,90,90,100],
+        borderColor: "green",
+        borderWidth:1
+      }
+    ]
+  },
+  options: {}
+}
+
+ReactDOM.render(<Chart {...data} />, document.getElementById('root'));
 
