b47621ca488f56ff650dc4e81fd2dbac4aace99e
Cristin O'Connor <cristin@cristin.io>
2017-11-06 09:31:10 -0600

    using onClick={() => this.handleClick(i)} is a beautiful way to bind this

--- src/app.js
+++ src/app.js
@@ -2,32 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.scss';
 
+// To make a fully-working game, we now need to check if one player has won the game, and alternate placing X and O in the squares. To check if someone has won, we’ll need to have the value of all 9 squares in one place, rather than split up across the Square components... the best solution here is to store this state in the Board component instead of in each Square – and the Board component can tell each Square what to display, like how we made each square display its index earlier.
+/***********************************************
+  When you want to aggregate data from multiple children or to have two child components communicate with each other, move the state upwards so that it lives in the parent component. The parent can then pass the state back down to the children via props, so that the child components are always in sync with each other and with the parent.
+***********************************************/
+
 class Square extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: null};
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState({ value: 'X' });
   }
   render() {
     return (
-      <span onClick={this.handleClick} className="square">
-        {this.state.value}
+      <span onClick={() => this.props.onClick()} className="square">
+        {this.props.value}
       </span>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      player: 'X'
+    };
   }
+  handleClick(i) {
+    console.log(this);
 
+  }
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
   render() {
-
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.player}`;
     return (
       <div>
         <p className="status">{status}</p>
