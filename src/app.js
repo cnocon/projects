21aaa79e1949eee5ac07768648283f649f26d76c
Cristin O'Connor <cristin@cristin.io>
2017-11-06 05:48:25 -0600

    used element variables to conditionally render a login or logout button component, which was handy

--- src/app.js
+++ src/app.js
@@ -14,21 +14,52 @@ function GuestGreeting(props) {
 }
 
 function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
+  return props.isLoggedIn ? <UserGreeting /> : <GuestGreeting />;
 }
 
+function LoginButton(props) {
+  return <button onClick={props.onClick}>Login</button>;
+}
+
+function LogoutButton(props) {
+  return <button onClick={props.onClick}>Logout</button>;
+}
+// In the example below, we will create a stateful component called LoginControl. It will render either <LoginButton /> or <LogoutButton /> depending on its current state. It will also render a <Greeting /> from the previous example:
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button = null;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+    return (
+      <div className="row">
+        <div className="col-sm"><Greeting isLoggedIn={isLoggedIn} /></div>
+        <div className="col-sm">{button}</div>
+      </div>
+    );
+  }
+}
 
 export class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <Greeting isLoggedIn={true} />
-        </div>
+        <LoginControl />
       </div>
     )
   }
