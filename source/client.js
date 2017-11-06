14ca9170091414de6e6cbf87138a03874ef3d362
Cristin O'Connor <cristin@cristin.io>
2017-11-06 02:50:22 -0600

    homicide data

--- source/client.js
+++ source/client.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-// import homicides from "../data/homicide-per-100000-2004-2015.csv";
 import homicides from "../data/csv-homicides_2014.csv";
-import {barData, a2c} from './utils/chartDataFormatter';
+import {a2c} from './utils/chartDataFormatter';
 
-// const data = barData(homicides);
 const data = a2c(homicides);
 
 ReactDOM.render(<App {...data} />, document.getElementById('root'));
--- source/utils/chartDataFormatter.js
+++ source/utils/chartDataFormatter.js
@@ -1,37 +1,35 @@
 import RandomColor from 'just.randomcolor';
 
-
-const formattedData = {
-	type:"bubble",
-	data: {
-		labels: ["Country"],
-		datasets: []
-	},
-	options: {
-    title: {
-      display: true,
-      text: "Homicide rate (per 100,000 people)"
-    },
-    scales: {
-      yAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: "Homicides per 100,000"
-        }
-      }],
-      xAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: "HDI (2015)"
-        }
-      }]
-    }
-	}
-}
-
 export function a2c(a){
+  const formattedData = {
+    type:"bubble",
+    data: {
+      labels: ["Country"],
+      datasets: []
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Homicide rate (per 100,000 people)"
+      },
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: "Homicides per 100,000"
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: "HDI (2015)"
+          }
+        }]
+      }
+    }
+  }
 
-	a.forEach(function(set) {
+  a.forEach(function(set) {
 		let color = new RandomColor().toRGBA().toCSS();
 		const thing = {label: set.Country, data: [{x: set['x'], y: set['y'], r: set['r']}], backgroundColor: color};
 
