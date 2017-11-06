5db7b800bf9bf2e6e9ecc2e67aecbd9bc5317dee
Cristin O'Connor <cristin@cristin.io>
2017-11-06 08:37:21 -0600

    basic tic tac toe components in place; prior to lifting up state, turn handling, and winner determination

--- src/app.js
+++ src/app.js
@@ -1,82 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CSSTransitionGroup } from 'react-transition-group'; // ES6
-// import Button from './bootstrap/button';
-// import Jumbotron from './bootstrap/jumbotron';
 import './app.scss';
-const messages = ['React', 'Re: React', 'Re:Re: React'];
 
-function UserGreeting(props) {
-  return <h1 className="float-left font-weight-light">Welcome</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1 className="float-left font-weight-light">Please Log In</h1>;
-}
-
-function Greeting(props) {
-  return props.isLoggedIn ? <UserGreeting /> : <GuestGreeting />;
-}
-
-function LoginButton(props) {
-  return <button {...props}>Log In</button>;
-}
-
-function LogoutButton(props) {
-  return <button {...props}>Log Out</button>;
-}
-// In the example below, we will create a stateful component called LoginControl. It will render either <LoginButton /> or <LogoutButton /> depending on its current state. It will also render a <Greeting /> from the previous example:
-class LoginControl extends React.Component {
+class Square extends React.Component {
   constructor(props) {
     super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
+    this.state = {value: null};
+    this.handleClick = this.handleClick.bind(this);
   }
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
+  handleClick() {
+    this.setState({ value: 'X' });
   }
   render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button = null;
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} className="btn btn-outline-danger ml-2 btn-lg"/>;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} className="btn btn-primary ml-2 btn-lg"/>;
-    }
     return (
-      <div className="card mx-auto mt-4 w-75">
-        <div className="card-header">
-          <Greeting isLoggedIn={isLoggedIn}/>
-          <div className="float-right">{button}</div>
-        </div>
-        {isLoggedIn &&
-          <div className="card-body mt-4">
-            {messages.length > 0 &&
-              <div className="card-text text-center">
-                <p className="h3 font-weight-normal">You have {messages.length} unread messages.</p>
-                <button className="btn btn-primary btn-lg mt-4 mb-4">View Mailbox</button>
-              </div>
-            }
+      <span onClick={this.handleClick} className="square">
+        {this.state.value}
+      </span>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+
+    const status = 'Next player: X';
+    return (
+      <div>
+        <p className="status">{status}</p>
+        <div className="game-board">
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
           </div>
-        }
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
+class Game extends React.Component {
 
+  render() {
+    return (
+      <div className="game">
+        <Board />
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 export class App extends React.Component {
   render() {
     return (
       <div className="container">
-
-          <LoginControl />
-
+        <Game />
       </div>
     )
   }
