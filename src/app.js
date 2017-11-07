de7aba2da8e1ba0e97fc665173fbc3c0fbb6499d
Cristin O'Connor <cristin@cristin.io>
2017-11-06 18:19:41 -0600

    [WIP] I dunno what i was doing

--- src/app.js
+++ src/app.js
@@ -20,16 +20,9 @@ class Board extends React.Component {
     };
   }
   handleClick(i) {
-    console.log(this.state.squares[i]);
     if (this.state.squares[i] == null) {
-      //use .slice() to copy the squares array instead of mutating the existing array.
-      // const squares = this.state.squares.slice();
-      // squares[i] = this.state.isXmove ? 'X' : 'O';
-      const key = `${i}`;
       const obj = new Object();
-      obj[key] = this.state.isXmove ? 'X' : 'O';
-
-
+      obj[i] = this.state.isXmove ? 'X' : 'O';
 
       this.setState({
         squares: Object.assign(this.state.squares, obj),
@@ -38,20 +31,15 @@ class Board extends React.Component {
     }
   }
   renderSquare(i) {
-    console.log('rendering square ' + i);
     return (
-      <Square
-        value={this.state.squares[i]}
-        data-value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
+      <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>
     );
   }
   render() {
     const winner = calculateWinner(this.state.squares);
     let status;
     if (winner) {
-      status = <div className="status bg-success"><span className="badge badge-success text-success mr-1">{`Player ${winner}`}</span> Wins!</div>;
+      status = <div className="status"><span className="badge badge-success text-success mr-1">{`Player ${winner}`}</span> Wins!</div>;
       $('.game-board').find('button').attr('disabled','disabled');
     } else {
       status = <div className="status">Next: <span className="badge badge-secondary">{`Player ${this.state.isXmove ? 'X' : 'O'}`}</span></div>;
@@ -61,19 +49,19 @@ class Board extends React.Component {
         {status}
         <div className="game-board">
           <div className="board-row">
-            <Square onClick={() => this.handleClick('0')} value={this.state.squares['0']}/>
-            <Square onClick={() => this.handleClick('1')} value={this.state.squares['1']}/>
-            <Square onClick={() => this.handleClick('2')} value={this.state.squares['2']}/>
+            <Square onClick={() => this.handleClick(0)} value={this.state.squares[0]}/>
+            <Square onClick={() => this.handleClick(1)} value={this.state.squares[1]}/>
+            <Square onClick={() => this.handleClick(2)} value={this.state.squares[2]}/>
           </div>
           <div className="board-row">
-            <Square onClick={() => this.handleClick('3')} value={this.state.squares['3']}/>
-            <Square onClick={() => this.handleClick('4')} value={this.state.squares['4']}/>
-            <Square onClick={() => this.handleClick('5')} value={this.state.squares['5']}/>
+            <Square onClick={() => this.handleClick(3)} value={this.state.squares[3]}/>
+            <Square onClick={() => this.handleClick(4)} value={this.state.squares[4]}/>
+            <Square onClick={() => this.handleClick(5)} value={this.state.squares[5]}/>
           </div>
           <div className="board-row">
-            <Square onClick={() => this.handleClick('6')} value={this.state.squares['6']}/>
-            <Square onClick={() => this.handleClick('7')} value={this.state.squares['7']}/>
-            <Square onClick={() => this.handleClick('8')} value={this.state.squares['8']}/>
+            <Square onClick={() => this.handleClick(6)} value={this.state.squares[6]}/>
+            <Square onClick={() => this.handleClick(7)} value={this.state.squares[7]}/>
+            <Square onClick={() => this.handleClick(8)} value={this.state.squares[8]}/>
           </div>
         </div>
       </div>
@@ -82,7 +70,6 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-
   render() {
     return (
       <div className="game">
@@ -95,8 +82,6 @@ class Game extends React.Component {
     );
   }
 }
-
-// ========================================
 
 export class App extends React.Component {
   render() {
